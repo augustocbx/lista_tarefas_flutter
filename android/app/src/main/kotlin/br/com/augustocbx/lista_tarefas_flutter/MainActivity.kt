656220c5846eecefb7e999a855e4d8df76217ef2
Augusto Xavier <augustocbx@gmail.com>
2019-09-05 18:57:54 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/augustocbx/lista_tarefas_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.augustocbx.lista_tarefas_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
